e8b5e85c64053288aee3ef735be827660add9226
Paul Puey <paul@edgesecure.co>
2018-09-06 01:11:48 -0700

    Make check_disk run as root
    Fixes failed test due to permissions problem.

--- nagios/install.sh
+++ nagios/install.sh
@@ -8,4 +8,9 @@ sudo apt-get install -y nagios-nrpe-server nagios-plugins
 git clone https://github.com/EdgeApp/edge-devops.git
 sudo cp ./edge-devops/nagios/bin/check_sync_failed* /usr/lib/nagios/plugins/
 sudo cp ./edge-devops/nagios/cfg/* /etc/nagios/nrpe.d
+
+sudo chown root:root /usr/lib/nagios/plugins/check_disk
+sudo chmod u+s /usr/lib/nagios/plugins/check_disk
+sudo chmod o+x /usr/lib/nagios/plugins/check_disk
+
 sudo /etc/init.d/nagios-nrpe-server restart
