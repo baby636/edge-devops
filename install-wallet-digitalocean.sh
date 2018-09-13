3165f5ceb48bf4ff13dbc240bbe5041376b174d3
Paul Puey <paul@edgesecure.co>
2018-09-12 18:34:00 -0700

    Digital Ocean specific deploy scripts

---
+++ install-wallet-digitalocean.sh
@@ -0,0 +1,8 @@
+## curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/install-wallet-digitalocean.sh | bash
+
+set -e
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-digitalocean.sh | bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-adduser.sh | bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/wallet/install.sh | bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/nagios/install.sh | sudo bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-aliases.sh | bash
---
+++ misc/install-digitalocean.sh
@@ -0,0 +1,1 @@
+sudo mv /boot/grub/menu.lst /
