07996ee24efada0f291f48a3221fe833ff7e587b
Paul Puey <paul@edgesecure.co>
2018-09-12 19:31:50 -0700

    Remove accidental azure script from digital ocean file

--- install-sync-digitalocean.sh
+++ install-sync-digitalocean.sh
@@ -5,7 +5,6 @@ sudo systemctl stop couchdb
 sleep 4
 set -e
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-digitalocean.sh | bash
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-azure.sh | sudo bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/sync/install.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/nagios/install.sh | sudo bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-aliases.sh | bash
--- install-wallet-digitalocean.sh
+++ install-wallet-digitalocean.sh
@@ -2,7 +2,6 @@
 
 set -e
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-digitalocean.sh | bash
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-adduser.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/wallet/install.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/nagios/install.sh | sudo bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-aliases.sh | bash
