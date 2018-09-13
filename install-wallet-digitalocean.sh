9a3135393b07d761220c8c6dbf9500e66a81fd8c
Paul Puey <paul@edgesecure.co>
2018-09-12 20:12:06 -0700

    Call addusers before script

--- install-wallet-digitalocean.sh
+++ install-wallet-digitalocean.sh
@@ -1,6 +1,7 @@
 ## curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/install-wallet-digitalocean.sh | bash
 
 set -e
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/addusers.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-digitalocean.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/wallet/install.sh | bash
 curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/nagios/install.sh | sudo bash
