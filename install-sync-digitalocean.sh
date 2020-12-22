291e49d9484dccee5ab05ee54871b9c83e7bb992
Samuel Holmes <sam@edge.app>
2020-12-21 17:19:28 -0800

    New sync server install scripts

--- install-sync-digitalocean.sh
+++ install-sync-digitalocean.sh
@@ -1,10 +1,12 @@
 ## curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/install-sync-digitalocean.sh | bash
 
+# Collect input
+read -s -p $'Enter CouchDB password: ' COUCH_PASSWORD
+export COUCH_PASSWORD
+
 echo "Stopping CouchDB in case it's running"
 sudo systemctl stop couchdb
 sleep 4
 set -e
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-digitalocean.sh | bash
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/sync/install.sh | bash
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/nagios/install.sh | sudo bash
-curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/install-aliases.sh | bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/install-couch-caddy-digitalocean.sh | bash
+curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/sync-server/install.sh | bash
---
+++ sync-server/install.sh
@@ -0,0 +1,33 @@
+## curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/sync-server/install.sh | bash
+
+# Install NodeJS/NPM LTS
+echo "Installing NodeJS/NPM..."
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash
+apt-get install -y nodejs
+
+# Install yarn
+echo "Installing Yarn..."
+npm i -g yarn
+
+# Install PM2
+echo "Installing PM2..."
+npm i -g pm2
+
+# Install ab-sync util
+echo "Installing ab-sync..."
+curl -L "https://github.com/EdgeApp/edge-sync-server/raw/master/shared-bin/ab-sync" -o /usr/local/bin/ab-sync
+chmod 755 /usr/local/bin/ab-sync
+
+# Install libgit2 util
+echo "Installing libgit2..."
+curl -L "https://github.com/EdgeApp/edge-sync-server/raw/master/shared-bin/libgit2.so.23" -o /usr/local/lib/libgit2.so.23
+chmod 755 /usr/local/lib/libgit2.so.23
+ldconfig
+
+# Install edge-sync-server
+echo "Provisioning sync server as edgy user..."
+sudo -i -u edgy bash -c "export COUCH_PASSWORD=$COUCH_PASSWORD; bash <(curl -o- \"https://raw.githubusercontent.com/EdgeApp/edge-devops/master/sync-server/install-sync-server.sh\")"
+
+# Setup PM2 to resurrect on startup
+echo "Setting up PM2 startup..."
+env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u edgy --hp /home/edgy
