c5e8cb149fc6db42c76269f5455c2c8b73ae1dbd
Samuel Holmes <sam@edge.app>
2021-02-17 11:19:12 -0800

    misc/user-security.sh for disabling root and edgy user SSH access

---
+++ misc/user-security.sh
@@ -0,0 +1,8 @@
+## curl -o- https://raw.githubusercontent.com/EdgeApp/edge-devops/master/misc/user-security.sh | bash
+
+# This will disable SSH access to root and edgy users.
+# This script should only be run after misc/addusers.sh and password change
+# for at least one administrative user.
+
+echo 'DenyUsers root edgy' > /etc/ssh/sshd_config.d/disable_users.conf
+systemctl restart sshd
