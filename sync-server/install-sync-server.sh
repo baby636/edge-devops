92776148917c870bdd2c4ac0cb930aa1463edb11
Samuel Holmes <sam@edge.app>
2021-04-08 17:28:13 -0700

    Remove yarn setup in favor of yarn prepare

--- sync-server/install-sync-server.sh
+++ sync-server/install-sync-server.sh
@@ -10,8 +10,6 @@ git clone https://github.com/EdgeApp/edge-sync-server.git
 echo "Installing edge-sync-server..."
 cd edge-sync-server
 yarn
-# Run setup for configuration
-yarn setup
 
 echo "Starting edge-sync-server..."
 pm2 start --name=edge-sync-server lib/index.js
