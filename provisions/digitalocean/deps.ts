d2ad0196bd5ad57bb5dcc02abfe2c0eb39454bb3
Samuel Holmes <sam@edge.app>
2021-04-21 12:59:43 -0700

    Added deps.ts file for lock.json generation

---
+++ provisions/digitalocean/deps.ts
@@ -0,0 +1,3 @@
+export * from "https://deno.land/x/cleaners@v0.3.6/mod.ts";
+export * from "https://deno.land/x/cliffy@v0.17.2/flags/mod.ts";
+export * from "https://deno.land/x/cliffy@v0.17.2/prompt/mod.ts";
